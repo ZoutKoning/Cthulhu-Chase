62ec6a437ed949065d015cc11a1b40292730949b
ZoutKoning <jfscyber9@gmail.com>
2023-04-18 20:38:14 -0400

    Background scrolls

--- game.py
+++ game.py
@@ -1,12 +1,12 @@
 import pygame, sys
 pygame.init()
 import math
+
 #window size Originally set to (800,600)
 #background image 1439 pixels wide, 730 pixels high
 FrameWidth = 1439
 FrameHeight = 730
 display = pygame.display.set_mode((FrameWidth, FrameHeight))
-
 clock = pygame.time.Clock()
 
 class Player:
@@ -45,68 +45,82 @@ player_bullets = []
 # background image size = 1439 pixels wide, 730 pixels high
 bg = pygame.image.load("CthulhuChaseBackground.png").convert()
 scroll = 0
-tiles = math.ceil(FrameWidth / bg.get_width()) + 1
+tiles = math.ceil(FrameWidth / bg.get_width())+1
 
 while True:
     clock.tick(60) #Manages speed of scrolling in pygame
-
-    #display.blit(bg, (0, 0))
     #display.fill((24,168,86))
-
-
-    #Background (GAME LOOP)
-    #while(player_right == True):
-    # Append image to the back of same image
-    i = 0
-    while(i < tiles):
-        display.blit(bg, (bg.get_width()*i + scroll, 0))
-        i += 1
-    #Frame for scrolling
-    scroll -= 6
-
-    #Reset the scroll Frame
-    if abs(scroll) > bg.get_width():
-        scroll = 0
     
-    # Player movement
-    #Move Left 
+#------- PLAYER MOVEMENT SECTION -------#
+
+    #Player has/moving Check variables
+    playerMovingRight = False
+    playerMovingLeft = False
     
     keys = pygame.key.get_pressed()
     pygame.draw.rect(display, (255, 255, 255), (100-display_scroll[0], 100-display_scroll[1], 16, 16))
     
+    #Move Left
     if keys[pygame.K_a]:
         display_scroll[0] -= 5
+        playerMovingLeft = True
+        playerMoved = 1
         for bullet in player_bullets:
             bullet.x += 5
 
     #Move Right    
     if keys[pygame.K_d]:
         display_scroll[0] += 5
-        
+        playerMovingRight = True
+        playerMoved = 1
         for bullet in player_bullets:
             bullet.x -= 5
     #Move Up        
     if keys[pygame.K_w]:
         display_scroll[1] -= 5
-        
         for bullet in player_bullets:
             bullet.x += 5
     #Move down
     if keys[pygame.K_s]:
         display_scroll[1] += 5
+        playerMoving = True
 
         for bullet in player_bullets:
             bullet.x -= 5
     mouse_x, mouse_y = pygame.mouse.get_pos()
-    #END GAME
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-            pygame.QUIT
 
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                player_bullets.append(PlayerBullet(player.x, player.y, mouse_x, mouse_y))
+
+#------- DRAW THINGS TO SCREEN SECTION -------#
+
+#--Background Scrolling--#
+
+    #Scrolls Right to Left
+    if(playerMovingRight == True):
+        i = 0
+        # Append image to the back of same image
+        display.blit(bg, (bg.get_width()*i + scroll, 0))
+        while(i < tiles):
+            display.blit(bg, (bg.get_width()*i + scroll, 0))
+            i += 1
+            #Frame for scrolling
+            scroll -= 3
+        #Reset the scroll Frame
+        if abs(scroll) > bg.get_width():
+            scroll = 0
+    
+    #Scrolls Left to Right 
+    if(playerMovingLeft == True):
+        i = 1439
+        # Append image to the fromt of same image
+        display.blit(bg, (bg.get_width()*i + scroll, 0))
+        while(i > tiles):
+            display.blit(bg, (bg.get_width()*i - scroll, 0))
+            i -=1
+            #Frame for scrolling 
+            scroll += 2
+            #Reset the scroll Frame
+        if abs(scroll) < bg.get_width():
+            scroll = 0
 
     #Draw player
     player.main(display)
@@ -115,4 +129,14 @@ while True:
         bullet.main(display)
     
     pygame.display.update()
+
+
+#------- END GAME -------#
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+            pygame.QUIT
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                player_bullets.append(PlayerBullet(player.x, player.y, mouse_x, mouse_y))
 
