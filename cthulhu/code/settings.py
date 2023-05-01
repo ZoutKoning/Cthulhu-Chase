bd342128e547b9de3dbe29f7bec0b504c4f50a0b
Joseph Suter <112408320+ZoutKoning@users.noreply.github.com>
2023-05-01 18:01:13 -0400

    Updated Level Maps (New Heights)
    new height level maps

--- cthulhu/code/settings.py
+++ cthulhu/code/settings.py
@@ -4,10 +4,10 @@ level_map1 = [
                 '                                                                                              ',
                 '                                                                                              ',
                 ' XX    XXX   X                                                        J                       ',
-                ' XX S            X                                                  XXX                       ',
+                ' XXS             X                                                  XXX                       ',
                 ' XXXX                X                                             XXX                        ',
                 ' XXXX                   X                  X                      XXXX                        ',
-                ' X              X  XX  XX  X  X  XX       XX                     XXXXX                     G  ',
+                ' X              X  XX  XX  X  X  XX       XX                     XXXXX                    G   ',
                 ' X P  X  X  X  XX                      XXXXX     X    X    X C XXXXXXLLLLLLLLLLLLLLLLLLXXXXXXX',
                 'E XXXXXLLXLLXLLXXLLLLLLLLLLLLLLLLLLLLLLLXXXXXLLLLLLLLLLLLLLXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
                 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX']
@@ -16,30 +16,37 @@ level_map2 = [
                 '                                                                                                      ',
                 '                                                                                                      ',
                 '                                                                                                      ',
+                '                                                                                              X   XXX ',
+                '                                                                                             XX   XXX ',
+                '                                                                                            XXX   XXX ',
                 '            XX                                                            XX              XXXXX   XXX ',
                 '                  X                                                      XX              XXXXX    XXX ',
                 ' XX    X              X                                                 XXX    X     X  XXXXXX    XXX ',
                 ' XXX          S         X         XX                                  XXXXX       X     XXXXXX      X ',
                 ' XXX          X  X  X  XX        XX                       X           XXXXX  XX         XXXXXX      X ',
                 ' X           XX                 XXX        X   X    X  X     X       XXXXXLLLLLLLLLLLLXXXXXXXXLLX   X ',
-                ' X P  X  X  XXXLLLLLLLLLLLLLLXXXXXXLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLXXXXXXXXXXLLLLLLLLLLLLXXXXXXXXX   X ',
-                'E XXXXXLLXLLXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX G      X',
-                'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX']#
+                ' X P GX  X  XXXLLLLLLLLLLLLLLXXXXXXLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLXXXXXXXXXXLLLLLLLLLLLLXXXXXXXXX   X ',
+                'E XXXXXLLXLLXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX        XL',
+                'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX']#
 
 level_map3 = [
                 '                                                                                                      ',
+                '                                                                                                      ',
                 'E                                                           X                                         ',
-                'X        X   XX     XXX                          X          X               J                         ',
-                'X XXX                     X    XLLLLXX   X   X        X     X               X                         ',
-                'X XX                           XXXXXX                      XX          X                              ',
-                'X XX                XX         X                       X   XX      X                                  ',
-                'X XXP  X  X   XXLLLLLLLLXX     X                   X       XX    XXLLLLLLLLLLLLLLLXLLLXXLLLX          ',
-                'X XXXXLLLLLLLLXXXXXXXXXXXX                X    XLLLLXXXXXXXXX   XXXXXXXXXXXXXXXXXXXXX  XXX      X     ',
-                'X XXXXXXXXXXXXXXXXXXXXXXXX            XLLLLLLLXXXXXXXX         XX                                     ',
+                'X        X   XX       X                                     X                                         ',
+                'X                                   J                       X                                         ',
+                'X XXX                     X    XLLLLXX   X   X   X    X     X               X                         ',
+                'X X                            XXXXXX                      XX          X                              ',
+                'X X                 XX         X                       X   XX      X                                  ',
+                'X X P  X  X   XXLLLLLLLLXX                                XXX    XXLLLLLLLLLLLLLLLXLLLXXLLLX          ',
+                'X XXXXLLLLLLLLXXXXXXXXXXXX                     XLLLXXXXXXXXXX   XXXXXXXXXXXXXXXXXXXXXXXX         X    ',
+                'X XXXXXXXXXXXXXXXXXXXXXXXX            XLLLXLLLXXXXXXXX         XX                                     ',
                 'X                       XXXLLLLXLLLLXLXXXXXXXXX         XXXXXXX                                       ',
+                'X                                                       XXXX                                          ',
                 'X                          S                          XXXXXX G   XLLLXLLLLLXLLLLXXLLLLXLLLXLLLLLLLLLLL',
                 'XXXLLLLXLLLLXXLLLLXLLLLXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX']
 
 tile_size = 64
 screen_width = 1200
 screen_height = 800
+#len(level_map1) * tile_size
